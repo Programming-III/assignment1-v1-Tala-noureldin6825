8cbb25f7400ecece4b5e7555576671f2caddd339
Tala-noureldin6825 <ntala2008@gmail.com>
2025-11-12 15:20:30 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,6 +3,14 @@
 
 #include "Animal.h"
 
+
+class Mammal:public animal{
+  private:
+  string furColor;
+  public:
+  Mammal();
+  Mammal(string name ,int age,bool isHungry , string furColor) :animal(name,age,isHungry),furColor(furColor);
+};
 //define mammal class here
 
 

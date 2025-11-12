7e274bdf720637dbe8a677ad1d60a37b1ead0c97
Tala-noureldin6825 <ntala2008@gmail.com>
2025-11-12 15:21:21 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -3,6 +3,14 @@
 
 #include "Animal.h"
 
+
+class Reptile: public animal{
+  private:
+  bool isVenemous;
+  public:
+  Reptile();
+  Reptile(string name,int age,bool isHungry, bool isVenemous):animal(string name,int age,bool isHungry)
+};
 //define reptile class here
 
 

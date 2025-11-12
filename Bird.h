866cbeb5f83863e083b5a74472a599b1081b8f7a
Tala-noureldin6825 <ntala2008@gmail.com>
2025-11-12 15:18:41 +0200

    Bird.h

--- Bird.h
+++ Bird.h
@@ -2,6 +2,15 @@
 #define BIRD_H
 
 #include "Animal.h"
+#include "animal.h
+
+class Bird: public animal{
+  private:
+  float wingSpan;
+  public:
+  Bird();
+  Bird(string name, int age, bool isHungry, float wingSpan):animal(string name,int age,bool isHungry),wingSpan(wingSpan);
+};
 
 //define class bird here 
 
